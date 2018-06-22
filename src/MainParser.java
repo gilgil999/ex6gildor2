4c20c69bb00c38db6aae80f6e923692ac93ad645
gilgil999 <gil.ronen.39.talpiot@gmail.com>
2018-06-23 02:28:16 +0300

    delete all

---
+++ src/MainParser.java
@@ -0,0 +1,7 @@
+public class MainParser {
+//needs to be singelton
+
+    public static Segment parse(String filetext){
+        return null;
+    }
+}
---
+++ src/Segment.java
@@ -0,0 +1,3 @@
+public abstract class Segment implements Checkable{
+
+}
