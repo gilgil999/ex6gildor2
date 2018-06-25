d0516a54865be3f0287893713dbd6bd7ad55de7b
DorTirosh <dor.tirosh.39.talpiot@gmail.com>
2018-06-26 02:04:10 +0300

    working 3

--- src/parser/MainParser.java
+++ src/parser/MainParser.java
@@ -12,37 +12,43 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainParser {
-//needs to be singelton
+	/*CONSTANTS-REGEX*/
+
+	/*GENERAL_CONSTANTS*/
+	private static final String INT = "-?\\s?\\d+";
+	private static final String STRING = "\"[^,\'\"\\\\]*\"";
+	private static final String CHAR = "\'[^,\'\"\\\\]?\'";
+	private static final String DOUBLE = "-?\\s?\\d+\\.\\d+";
 	private static final String space = "\\s*";
 	private static final String FINAL = "(?:final\\s+)";
-	private static final String STRING_CONTENT = "\"[^,\'\"\\\\]*\"";
 	private static final String BOOLEAN_CONTENT = "(?:true|false)";
 	private static final String IF_WHILE = "(?:if|while)";
 	private static final String OPEN_FINISH = ".*(?:\\{\\s*)$";
 	private static final String CLOSE = "\\s*}\\s*";
 	private static final String SINGLE_LINE = ".*(?:;\\s*)$";
-	private static final String VALID_NUMBER = "(?:-?\\s?\\d+(?:.\\d+)?)";
-	private static final String INT = "-?\\s?\\d+";
-	private static final String STRING = "\"[^,\'\"\\\\]*\"";
-	private static final String CHAR = "\'[^,\'\"\\\\]?\'";
-	private static final String DOUBLE = "-?\\s?\\d+\\.\\d+";
-	private static final String NAME_VAR_VALDIATION = "(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)";
-	public static final String CONTENT =  "(?:" + VALID_NUMBER + "|" + CHAR + "|" + BOOLEAN_CONTENT + "|" + STRING_CONTENT + "|" + NAME_VAR_VALDIATION + ")"; // (?:(?:\d+(?:.\d+)?)|(?:true|false)|"\S*"|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))
+	private static final String VALID_NUMBER = "(?:" + INT + "|" + DOUBLE +")";
+	private static final String RETURN = space + "return\\s*;" + space;
+
+	/*BASIC EXPRESSIONS*/
+	public static final String NAME_VAR_VALDIATION = "(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)";
+	public static final String CONTENT =  "(?:" + VALID_NUMBER + "|" + CHAR + "|" + BOOLEAN_CONTENT + "|" + STRING +  ")"; // (?:(?:\d+(?:.\d+)?)|(?:true|false)|"\S*"|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))
 	private static final String CHECK_TYPE ="(?:int|String|char|double|boolean)";
 	private static final String CONDITION = "(?:" + VALID_NUMBER + "|" + BOOLEAN_CONTENT + "|" + NAME_VAR_VALDIATION + ")"; // (?:(?:\d+(?:.\d+)?)|(?:true|false)|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))
 	private static final String CONDITION_LINE = "(?:" + space + IF_WHILE + space + "\\((?:"+ space + CONDITION + space + "(?:\\|\\||&&))*" + space + CONDITION + space + "\\)" + space + "\\{" + space + ")"; // (?:\s*(?:if|while)\s*\((?:\s*(?:(?:\d+(?:.\d+)?)|(?:true|false)|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))\s*(?:\|\||&&))*\s*(?:(?:\d+(?:.\d+)?)|(?:true|false)|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))\s*\)\s*{\s*)
 	private static final String NAME_METHOD_VALDIATION = "[a-zA-Z]+[a-zA-Z0-9_]*";
-	private static final String CHECK_CONDITION = "(?:(?:\\s)*(?:true|false|"+NAME_VAR_VALDIATION+"|" + VALID_NUMBER+"))";
+
+	/*COMPLEX LINES*/
 	private static final String PARAMS = "(?:" +space+ FINAL + "?"  + space+ CHECK_TYPE + "\\s+" + NAME_VAR_VALDIATION + space + ")"; // (?:\s*(?:int|String|char|double|boolean)\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*)
-	public static final String CHECK_PARAM ="(?:[(](?:" + PARAMS + ",)*" + PARAMS + "?\\s*[)]"+space+"\\{\\s*)"; // (?:[(](?:(?:\s*(?:final\s+)?\s*(?:int|String|char|double|boolean)\s+(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*),)*(?:\s*(?:final\s+)?\s*(?:int|String|char|double|boolean)\s+(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*)[)]\s*\{\s*)
-	public static final String CHECK_FUNC_LINE = "(?:" +space + "void"  + "\\s+" + NAME_METHOD_VALDIATION + space + CHECK_PARAM + ")"; // (?:\s*(?:void|(?:int|String|char|double|boolean))\s+[a-zA-Z]+[a-zA-Z0-9_]*\s*(?:[(](?:(?:\s*(?:int|String|char|double|boolean)\s+(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*),)*(?:\s*(?:int|String|char|double|boolean)\s+(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*)[)]\s*\{\s*))
-	public static final String ASSIGNMENT = "(?:" + space + "(" + NAME_VAR_VALDIATION + ")" + space + "=" + space + "(" + CONTENT + ")" + space + ")"; //(?:\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*=\s*(?:(?:\d+(?:.\d+)?)|(?:true|false)|"\S*"|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))\s*)
-	public static final String CHECK_ASSIGNMENT = FINAL + "?" + space + CHECK_TYPE + "\\s+" + "(?:(?:(?:\\s*" + ASSIGNMENT + "\\s*|" + NAME_VAR_VALDIATION + "\\s*),)*\\s*" + "(?:\\s*" + ASSIGNMENT + "\\s*|\\s*" + NAME_VAR_VALDIATION + "\\s*);\\s*)"; // (?:final\s+)?\s*(?:int|String|char|double|boolean)\s+(?:(?:(?:\s*(?:\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*=\s*(?:(?:\d+(?:.\d+)?)|(?:true|false)|"\S*"|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))\s*)\s*|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*),)*\s*(?:\s*(?:\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*=\s*(?:(?:\d+(?:.\d+)?)|(?:true|false)|"\S*"|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))\s*)\s*|\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*);\s*)
-	public static final String ASSIGN_WITHOUTH_DEC = space +",?(" + NAME_VAR_VALDIATION + ")" + space + "(?:,|;)";
-	private static final String RETURN = "\\s*return\\s*;\\s*";
-	public static final String FUNC_CALL = space  + NAME_METHOD_VALDIATION + space +"\\((?:" + space + NAME_VAR_VALDIATION +space+ "|" +space+ CONTENT + space + ",)*" + space + "(?:" + NAME_VAR_VALDIATION  + "|" + CONTENT + ")?" + space + "\\)" +space +";" + space;
-
-
+	private static final String CHECK_PARAM ="(?:[(](?:" + PARAMS + ",)*" + PARAMS + "?\\s*[)]"+space+"\\{\\s*)"; // (?:[(](?:(?:\s*(?:final\s+)?\s*(?:int|String|char|double|boolean)\s+(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*),)*(?:\s*(?:final\s+)?\s*(?:int|String|char|double|boolean)\s+(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*)[)]\s*\{\s*)
+	private static final String CHECK_FUNC_LINE = "(?:" +space + "void"  + "\\s+" + NAME_METHOD_VALDIATION + space + CHECK_PARAM + ")"; // (?:\s*(?:void|(?:int|String|char|double|boolean))\s+[a-zA-Z]+[a-zA-Z0-9_]*\s*(?:[(](?:(?:\s*(?:int|String|char|double|boolean)\s+(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*),)*(?:\s*(?:int|String|char|double|boolean)\s+(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*)[)]\s*\{\s*))
+	private static final String ASSIGNMENT = "(?:" + space + "(" + NAME_VAR_VALDIATION + ")" + space + "=" + space + "(" + CONTENT + ")" + space + ")"; //(?:\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*=\s*(?:(?:\d+(?:.\d+)?)|(?:true|false)|"\S*"|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))\s*)
+	private static final String CHECK_ASSIGNMENT = space +FINAL + "?" + space + CHECK_TYPE + "\\s+" + "(?:(?:(?:\\s*" + ASSIGNMENT + "\\s*|" + NAME_VAR_VALDIATION + "\\s*),\\s*)*\\s*" + "(?:\\s*" + ASSIGNMENT + "\\s*|\\s*" + NAME_VAR_VALDIATION + "\\s*);\\s*)"; // (?:final\s+)?\s*(?:int|String|char|double|boolean)\s+(?:(?:(?:\s*(?:\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*=\s*(?:(?:\d+(?:.\d+)?)|(?:true|false)|"\S*"|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))\s*)\s*|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*),)*\s*(?:\s*(?:\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*=\s*(?:(?:\d+(?:.\d+)?)|(?:true|false)|"\S*"|(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*))\s*)\s*|\s*(?:_[a-zA-Z0-9_]+|[a-zA-Z]+[a-zA-Z0-9_]*)\s*);\s*)
+	private static final String ASSIGN_WITHOUTH_DEC = space +",?(" + NAME_VAR_VALDIATION + ")" + space + "(?:,|;)";
+	private static final String FUNC_CALL = space  + NAME_METHOD_VALDIATION + space +"\\((?:" + space + NAME_VAR_VALDIATION +space+ "|" +space+ CONTENT + space + ",)*" + space + "(?:" + NAME_VAR_VALDIATION  + "|" + CONTENT + ")?" + space + "\\)" +space +";" + space;
+	/**
+	 * enum describing the possible types of objects in javas.
+	 */
+	public enum varType {DOUBLE, INT, STRING, CHAR, BOOLEAN,UNKNOWN}
 	/**
 	 * this function receive ArrayList of Strings and delete extra spaces, empty lines and comment lines
 	 * @param text the text you want to parse
@@ -64,12 +70,6 @@ public class MainParser {
 		return newText;
 	}
 
-
-    public enum varType {DOUBLE, INT, STRING, CHAR, BOOLEAN,UNKNOWN}
-    
-
-
-
     public static GlobalSegment parse(ArrayList<String> lines) throws TypeOneException{
         RawLine[] parsedlines = readlines(lines);
         GlobalSegment globalSegment = new GlobalSegment();
@@ -88,7 +88,7 @@ public class MainParser {
                     //we are already in the globalsegment
                     //todo exception
                     throw new TypeOneException();
-                    ////system.out.println("a closing without an opening");
+                    ////System.out.println("a closing without an opening");
                 else
                     currentParent = currentParent.getParent();
             }else{
@@ -103,17 +103,19 @@ public class MainParser {
         if(currentParent.getParent()!=null)
             throw new TypeOneException();
             //todo exception
-            ////////system.out.println("an opening without a closing");
+            System.out.println("an opening without a closing");
         if(globalSegment==currentParent)
             //should apply, not a neccessary check
             return globalSegment;
-        ////system.out.println("currentparent is not globalsegment");
+        System.out.println("currentparent is not globalsegment");
         throw new TypeOneException();
 
     }
 
-
-    private static RawLine[] readlines(ArrayList<String> parsed_text) throws BadLineException {
+	/*
+	function that receives array list of (parsed) line Strings and return list of raw line - lines that describe what this line does.
+	 */
+	private static RawLine[] readlines(ArrayList<String> parsed_text) throws BadLineException {
         RawLine[] parsedlines=new RawLine[parsed_text.size()];
         int counter = 0;
         for (String row:parsed_text){
@@ -130,6 +132,9 @@ public class MainParser {
 
     }
 
+	/*
+	function that receives String line  and return new raw line - line that describe what this line does.
+	 */
     public static RawLine readline (String line) throws BadLineException {
 		Pattern pattern = Pattern.compile(OPEN_FINISH);
 		Matcher matcher = pattern.matcher(line);
@@ -150,12 +155,13 @@ public class MainParser {
 		throw new BadLineException();
     }
 
-    public static RawLine treatSingle(String line)throws BadLineException {
+    /*conceive line that ends with ; and return matching raw line*/
+    private static RawLine treatSingle(String line)throws BadLineException {
 		Pattern pattern = Pattern.compile(CHECK_ASSIGNMENT);
 		Matcher matcher = pattern.matcher(line);
 		if (matcher.matches()){ // this is assignment line (with deceleration)
 			ArrayList<VarOperation>  vars = new ArrayList<>();
-			////system.out.println("this is declaration line");
+			System.out.println("this is declaration line");
 			boolean isFinal = false;
 			varType type;
 			pattern = Pattern.compile(NAME_METHOD_VALDIATION);
@@ -164,7 +170,7 @@ public class MainParser {
 			if(matcher.group(0).equals("final")){
 				isFinal = true;
 				matcher.find();
-				////system.out.println("the vars are final");
+				System.out.println("the vars are final");
 			}
 			type = checkType(matcher.group(0));
 			line = line.substring(matcher.end());
@@ -182,8 +188,8 @@ public class MainParser {
                     name = matcher.group(1);
                     content = matcher.group(2);
                     variableType = recognizeType(content);
-                    ////system.out.println("assign " + content + "(type " + variableType + ") to " + name + "(type " + type + ")");
-                    if (type != varType.UNKNOWN) {
+                    System.out.println("assign " + content + "(type " + variableType + ") to " + name + "(type " + type + ")");
+                    if (variableType != varType.UNKNOWN) {
                         content = null;
                     }
                     VarOperation operation = new VarOperation(variableType, type, content, name);
@@ -193,11 +199,11 @@ public class MainParser {
                 else if(matcher1.lookingAt()){
 			        matcher1.reset();
                     matcher1.find();
-                    ////system.out.println();
+                    System.out.println();
                     name = matcher1.group(1);
                     content = null;
                     variableType = varType.UNKNOWN;
-                    ////system.out.println("declare " + name + "(type " + type + ")");
+                    System.out.println("declare " + name + "(type " + type + ")");
                     VarOperation operation= new VarOperation(variableType, type,content,name);
                     vars.add(operation);
                     line = line.substring(matcher1.end());
@@ -211,7 +217,7 @@ public class MainParser {
 		matcher = pattern.matcher(line);
 		if(matcher.matches()){
 			ArrayList<VarOperation> vars = new ArrayList<>();
-			////system.out.println("this is assignment line");
+			System.out.println("this is assignment line");
 			boolean isFinal = false;
 			String name = matcher.group(1);
 			String content = matcher.group(2);
@@ -219,7 +225,7 @@ public class MainParser {
 			if (variableType != varType.UNKNOWN){
 				content = null;
 			}
-			////system.out.println("assign " + content + "(type " + variableType + ") to " + name + "(type UNKNOWN");
+			System.out.println("assign " + content + "(type " + variableType + ") to " + name + "(type UNKNOWN");
 			VarOperation operation= new VarOperation(variableType,varType.UNKNOWN,content,name);
 			vars.add(operation);
 			return new VarDecelaration(isFinal,vars);
@@ -227,29 +233,29 @@ public class MainParser {
 		pattern = Pattern.compile(RETURN);
 		matcher = pattern.matcher(line);
 		if (matcher.matches()){
-			////system.out.println("this is return line");
+			System.out.println("this is return line");
 			return new RawReturn();
 		}
 		pattern = Pattern.compile(FUNC_CALL);
 		matcher = pattern.matcher(line);
 		if (matcher.matches()) { // this is function call
 			ArrayList<VarInstance> vars = new ArrayList<>();
-			////system.out.println("this is call to function");
+			System.out.println("this is call to function");
 			pattern = Pattern.compile("(?:" + CONTENT + "|" +NAME_VAR_VALDIATION +")");
 			matcher = pattern.matcher(line);
 			matcher.find();
 			String methodName = matcher.group(0);
-			////system.out.println("function name is " + methodName);
-			////system.out.println("the vars you got: ");
+			System.out.println("function name is " + methodName);
+			System.out.println("the vars you got: ");
 			while (matcher.find()){
 				String varName = matcher.group(0);
 				varType type = recognizeType(varName);
 				if (type == varType.UNKNOWN){
-                    ////system.out.println("variable named: " + varName);
+                    System.out.println("variable named: " + varName);
                     vars.add(new VarInstance(varName,varType.UNKNOWN));
                     continue;
                 }
-                ////system.out.println("type " + type+ " variable");
+                System.out.println("type " + type+ " variable");
 				vars.add(new VarInstance(null,type));
 			}
 			return new RawFuncCall(methodName,vars);
@@ -257,6 +263,8 @@ public class MainParser {
 		throw new BadLineException();
 	}
 
+	/*this function receive a String and recognize the type of his content.
+	 (for example : for the input "4" the function return INT */
 	private static varType recognizeType(String content) {
 		Pattern pattern = Pattern.compile(INT);
 		Matcher matcher = pattern.matcher(content);
@@ -287,7 +295,8 @@ public class MainParser {
 		return type;
 	}
 
-	public static RawLine treatOpen (String line) throws BadLineException {
+	/*conceive line that ends with ; and return matching raw line*/
+	private static RawLine treatOpen (String line) throws BadLineException {
 		Pattern pattern = Pattern.compile(CONDITION_LINE);
 		Matcher matcher = pattern.matcher(line);
 		if (matcher.matches()){ // this is condition
@@ -302,17 +311,17 @@ public class MainParser {
 	}
 
 	private static OpenFunction treatFunction(String line) {
-		////system.out.println("this is a function");
+		System.out.println("this is a function");
 		String name;
 		Pattern pattern = Pattern.compile(NAME_METHOD_VALDIATION);
 		Matcher matcher = pattern.matcher(line);
 		matcher.find(); //find void
 		matcher.find();// find the name of the function
 		name = line.substring(matcher.start(),matcher.end());
-		////system.out.println("the name of the function is: " + name);
+		System.out.println("the name of the function is: " + name);
 		FunctionObj functionObj = new FunctionObj(name);
 		String [] params = line.split("\\(")[1].split(",");
-		////system.out.println("the vars you got:");
+		System.out.println("the vars you got:");
 		for (String param: params) {
 			boolean isFinal = false;
 			String varName;
@@ -332,26 +341,26 @@ public class MainParser {
 			varName = param.substring(matcher.start(),matcher.end());
 			functionObj.addVar(varName,type,isFinal);
 //			System.out.print("var name: "  + varName + "   var type: " + type + "   is it final? : ");
-			////system.out.println(isFinal);
+			System.out.println(isFinal);
 
 		}
 		return new OpenFunction(functionObj);
 	}
 
 	private static OpenCondition treatCondition(String line) {
-		////system.out.println("this is condition");
+		System.out.println("this is condition");
 		OpenCondition rawLine = new OpenCondition();
 		String splited_line = line.split("\\(")[1];
 		Pattern pattern = Pattern.compile(NAME_VAR_VALDIATION);
 		Matcher matcher = pattern.matcher(splited_line);
-		////system.out.println("the names you got:");
+		System.out.println("the names you got:");
 		while (matcher.find()){
 			int start = matcher.start();
 			int end= matcher.end();
 			String name = splited_line.substring(start,end);
 			if ((!name.equals("true"))&&(!name.equals("false"))) {
 				rawLine.addName(name);
-				////system.out.println(name);
+				System.out.println(name);
 			}
 		}
 		return rawLine;
